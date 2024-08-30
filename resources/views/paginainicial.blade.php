7692511c91af5afab6aefc052efd4611be011193
rafaelperezfd <ds.42111580821@gmail.com>
2024-08-30 15:35:48 -0300

    Co-authored-by: Daniel Pietro <DanielDPietro@users.noreply.github.com> Co-authored-by: Otávio dos Santos Carvalho <OtavioCarvalho73@users.noreply.github.com> Co-authored-by: Eduardo Novaes Paganotti <EduardoPaganotti@users.noreply.github.com> Co-authored-by: Ian Erler Mendes <ia717@users.noreply.github.com>

--- resources/views/paginainicial.blade.php
+++ resources/views/paginainicial.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <!-- Conteúdo Principal -->
-    <div id="main-content" class="flex-1 p-10 bg-gray-50 text-black transition-all duration-300 ml-12">
+    <div id="main-content" class="flex-1 p-10 bg-gray-50 dark:bg-blue text-black transition-all duration-300 ml-12">
         <h1 class="text-3xl font-bold mb-4">Bem-vindo ao Conteúdo Principal</h1>
         <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Sed nec ex at nunc fermentum congue.</p>
         <p class="mb-4">Praesent condimentum nunc nec felis tempor, non cursus nisl dignissim. Curabitur in libero et orci euismod dignissim a vel lorem.</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    plugins: [forms],
-     darkMode: 'class' 
-};
