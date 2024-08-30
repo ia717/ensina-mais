1b5a3dc530b1cddd6e4e5b8ead12330371497ba3
LeonardoMartins016 <ds.45908023841@gmail.com>
2024-08-30 11:13:25 -0300

    adicionei os ícones no deshboard

--- app/Filament/Resources/LessonResource.php
+++ app/Filament/Resources/LessonResource.php
@@ -17,7 +17,7 @@ class LessonResource extends Resource
 {
     protected static ?string $model = Lesson::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
 
     public static function form(Form $form): Form
     {
--- app/Filament/Resources/TopicResource.php
+++ app/Filament/Resources/TopicResource.php
@@ -17,7 +17,7 @@ class TopicResource extends Resource
 {
     protected static ?string $model = Topic::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-book-open';
 
     public static function form(Form $form): Form
     {
