41564cb956e48ad4358dbdd0f0a2aaa236acaad3
Caio Pellegrini <caiopellegrini97@gmail.com>
2024-09-02 17:25:19 -0300

    ajustes no front para tailwind ser aplicado (ainda falta coisas)

--- resources/views/login2.blade.php
+++ resources/views/login2.blade.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Para Gabaritar</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="icon" href="{{asset('/imagens/mini logo nova(1).png')}}" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- trocar a cor pela a que esta no figma -->
-    <!-- @vite('resources/css/app.css') -->
     <script src="https://unpkg.com/scrollreveal">
 
     </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,31 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+  darkMode: 'class', // Mude para media para alterar com o sistema do usuario
+
+  theme: {
+    extend: {
+      fontFamily: {
+        
+      },
+
+      backgroundColor: {
+        
+      },
+
+      colors: {
+        
+      },
+    },
+  },
+
+  plugins: [forms],
+};
