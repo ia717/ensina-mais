3043147e9646b21354afde324cbac63c2eddb597
Caio Pellegrini <caiopellegrini97@gmail.com>
2024-09-03 15:47:45 -0300

    adicionei link de admin na navbar da welcome.blade, para facilitar durante dev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -915,6 +915,11 @@
                     </div>
                     @if (Route::has('login'))
                     <nav class="-mx-3 flex flex-1 justify-end">
+                        <a
+                            href="{{ url('/admin') }}"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                            Admin
+                        </a>
                         @auth
                         <a
                             href="{{ url('/dashboard') }}"
