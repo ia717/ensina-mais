cc9bdd30e7a6e852c71cb2caa93be67091caa8a9
caio-pellegrini <caiopellegrini97@gmail.com>
2024-09-26 12:10:10 -0300

    Resposta de questão funcionando

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -15,4 +15,18 @@ class QuestionController extends Controller
 
         return view('questao', compact('questions'));
     }
+
+    public function checkAnswer(Question $question, Request $request)
+    {   
+        $inputField = 'options_'.$question->id;      
+        $correctAnswerId = $question->answers->where('is_correct', true)->first()->id;
+        $studentAnswerId = intval($request->input($inputField));
+        // falta terminar essa lógica
+        // no futuro, salvar no banco de dados a resposta do aluno
+        if ($studentAnswerId == $correctAnswerId) { 
+            return view('questaor');
+        } else {
+            return view('questaow');
+        }
+    }
 }
