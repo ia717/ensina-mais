b98095113b5f18932797abd46ca125758ddfa4ee
caio-pellegrini <caiopellegrini97@gmail.com>
2024-09-26 12:29:20 -0300

    ajustes redacao

--- resources/views/redacao.blade.php
+++ resources/views/redacao.blade.php
@@ -1,22 +1,13 @@
+<x-app-layout> 
 
-    <x-app-layout> 
-
-    
-
-    <main class="flex-1 flex justify-around items-center">
+    <div class="flex-1 flex justify-around items-center">
         <div class="text-center w-full p-8 md:p-0 md:w-auto">
             <div class="text-gray-500 text-lg mb-5 flex items-center">Utilize os filtros para buscar temas de redação</div>
             <span class="material-icons text-gray-400 text-5xl"></span>
         </div>
-    </main>
-
-
-    @include('filtroredacao')
-    
-  
     </div>
+    
+    {{-- O código no arquivo abaixo está impedindo que o sidebar funcione! --}}
+    @include('filtroredacao')
 
-
-    </x-app-layout>
-
-
+</x-app-layout>
