1d83339db8068ce36357a907d9c189b2e143d1d7
LeonardoMartins016 <ds.45908023841@gmail.com>
2024-09-27 09:35:17 -0300

    fiz o select de vestibular

--- resources/views/redacao.blade.php
+++ resources/views/redacao.blade.php
@@ -18,14 +18,13 @@
 
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="vestibular">Vestibular</label>
-                <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="vestibular">
-                    <option>ENEM</option>
-                    <option>Unesp</option>
-                    <option>FUVEST</option>
-                    <option>Outros</option>
-                </select>
+                
             </div>
-
+                <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="vestibular">
+                    @foreach ($vestibulares as $vestibular)
+                    $vestibular->name
+                    @endforeach
+                </select>
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="modelo">Modelo</label>
                 <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="modelo">
