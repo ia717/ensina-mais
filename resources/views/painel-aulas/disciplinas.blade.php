5882f5f5a945456dc11a10f1bf027a19fc4d8aac
caio-pellegrini <caiopellegrini97@gmail.com>
2024-10-11 07:39:36 -0300

    breadcrumbs responsivo

--- resources/views/painel-aulas/disciplinas.blade.php
+++ resources/views/painel-aulas/disciplinas.blade.php
@@ -7,8 +7,8 @@
             @foreach ($categories as $category)
                 <div>
                     <div class="flex items-center text-2xl font-bold mb-2.5">
-                        <span class="text-3xl" style="color: {{ $category->color }}">|</span><h2>{{ $category->name }}</h2>
-                    </div>
+                        <span class="inline-block w-1 h-6 mr-2" style="background-color: {{ $category->color }}"></span><h2>{{ $category->name }}</h2>
+                    </div>            
 
                     @if ($category->disciplines->isEmpty())
                         <p>Nenhuma disciplina cadastrada</p>
@@ -17,7 +17,7 @@
                     <div class="flex flex-wrap gap-5">
                         @foreach ($category->disciplines as $discipline)
                             <a href="{{ route('topicos', $discipline) }}" style="background-color: {{ $category->color }}"
-                                class="w-40 h-32 rounded-lg flex flex-col items-center justify-center shadow-md text-center cursor-pointer transition-all duration-300 hover:-translate-y-1.5 hover:brightness-75">
+                                class="w-32 h-28 sm:w-40 sm:h-32  rounded-lg flex flex-col items-center justify-center shadow-md text-center cursor-pointer transition-all duration-300 hover:-translate-y-1.5 hover:brightness-75">
                                 <i class="text-3xl mb-2.5 fas fa-calculator"></i> {{-- adicionar icon do database --}}
                                 <span class="text-md font-semibold">{{ $discipline->name }}</span>
                             </a>
--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -1,12 +1,12 @@
 @unless ($breadcrumbs->isEmpty())
     <nav class="flex" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-1 md:space-x-2">
+        <ol class="inline-flex items-center space-x-1 md:space-x-2 flex-wrap">
             @foreach ($breadcrumbs as $breadcrumb)
                 @if ($breadcrumb->url && !$loop->last)
                     @if ($loop->first)
                         <li class="inline-flex items-center">
                             <a href="{{ $breadcrumb->url }}"
-                                class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white break-words truncate">
                                 <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                     fill="currentColor" viewBox="0 0 20 20">
                                     <path
@@ -24,7 +24,7 @@
                                         stroke-width="2" d="m1 9 4-4-4-4" />
                                 </svg>
                                 <a href="{{ $breadcrumb->url }}"
-                                    class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">{{ $breadcrumb->title }}</a>
+                                    class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white break-words truncate">{{ $breadcrumb->title }}</a>
                             </div>
                         </li>
                     @endif
@@ -36,7 +36,7 @@
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="m1 9 4-4-4-4" />
                             </svg>
-                            <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">{{ $breadcrumb->title }}</span>
+                            <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400 break-words truncate">{{ $breadcrumb->title }}</span>
                         </div>
                     </li>
                 @endif
