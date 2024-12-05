dc3ff6c3f0735b243ef07f4462dd0eb8cf0c653a
caio-pellegrini <caiopellegrini97@gmail.com>
2024-12-05 10:38:52 -0300

    remove comentário na home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-    {{-- Breadcrumbs::render('home') apenas um teste --}}
     <div class="flex w-full h-max">
         <img src="{{ asset('images\home\banner.png') }}" alt="">
     </div>
