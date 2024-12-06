90bdf3f8ef76b64dfe8f3e284909e0936e538648
caio-pellegrini <caiopellegrini97@gmail.com>
2024-12-06 15:32:17 -0300

    atualiza tailwind dependency to v4-beta

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from 'tailwindcss';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +8,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss()
     ],
     css: {
         postcss: {
